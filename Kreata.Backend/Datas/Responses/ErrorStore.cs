2962482ba1573d2e406823d12ed3e8b5fc388774
c9czamat <c9czamat@vasvari.org>
2024-10-24 10:15:35 +0200

    Response osztályok (órai munka)

---
+++ Kreata.Backend/Datas/Responses/ErrorStore.cs
@@ -0,0 +1,13 @@
+﻿namespace Kreata.Backend.Datas.Responses
+{
+    public class ErrorStore
+    {
+        public ErrorStore()
+        {
+            Error=string.Empty;
+        }
+        public string Error {  get; set; }=string.Empty;
+        public bool HasError=>!string.IsNullOrEmpty(Error);
+    }
+    
+}
